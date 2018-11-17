c08891fd844f150feb56e921c5252de6d48716d9
Iqra Muhammad <iqra.muhammad15@ce.ceme.edu.pk>
2018-11-17 21:59:07 +0500

    Improve touch support in grid paint activity

--- js/events.js
+++ js/events.js
@@ -1,4 +1,4 @@
-﻿//Copyright (c) 2013, Playful Invention Company.
+//Copyright (c) 2013, Playful Invention Company.
 
 //Permission is hereby granted, free of charge, to any person obtaining a copy
 //of this software and associated documentation files (the "Software"), to deal
@@ -26,70 +26,60 @@
 
 var onStart, onMove, onEnd;
 var zoom;
-var shiftTop;
+var touchScreen = false;
 
 function eventInit(){
-	window.onmousedown = evMousedown;	
-	frame.onmousemove = evMousemove;	
-	frame.onmouseup = evMouseup;
-	var wsize = window.innerWidth;
-	var wtop = 55;
-	if (wsize <= 480) {
-		zoom = 0.353;
-		wtop = 150;
-		shiftTop = -85;
-	} else if (wsize <= 854) {
-		zoom = 0.565;
-		wtop = 90;
-		shiftTop = -40;
-	} else if (wsize <= 960) {
-		zoom = 0.645;
-		wtop = 90;
-		shiftTop = -32;
-	} else if (wsize <= 1024) {
-		zoom = 0.95;
-		wtop = 60;
-		shiftTop = 0;
+	touchScreen = ("ontouchstart" in document.documentElement);
+	if (touchScreen) {
+		document.getElementById("canvas").addEventListener("touchstart", evMousedown, false);
+		frame.addEventListener("touchmove", evMousemove, false);
+		frame.addEventListener("touchend", evMouseup, false);
 	} else {
-		zoom = 1.10;
-		wtop = 55;	
-		shiftTop = 0;
+		document.getElementById("canvas").onmousedown = evMousedown;
+		frame.onmousemove = evMousemove;
+		frame.onmouseup = evMouseup;
 	}
+	var wsize = document.body.clientHeight-55;
+	zoom = wsize/748;
 	document.getElementById("canvas").style.zoom = zoom;
 	var useragent = navigator.userAgent.toLowerCase();
 	if (useragent.indexOf('chrome') == -1) {
 		document.getElementById("canvas").style.MozTransform = "scale("+zoom+")";
 		document.getElementById("canvas").style.MozTransformOrigin = "0 0";
 		document.getElementById("canvas").style.width = "1024px";
-		document.getElementById("canvas").style.height = "748px";	
-	} else {
-		document.getElementById("canvas").style.top = wtop + "px";
-		shiftTop = 0;
+		document.getElementById("canvas").style.height = "748px";
 	}
 }
 
 function evMousedown(e){
-	e.preventDefault(); 
+	e.preventDefault();
+	if (touchScreen) e = e.touches[0];
 	var x=localx(e.clientX), y=localy(e.clientY);
 	onStart(x,y);
+	// HACK: Force refresh on Android
+	if (/Android/i.test(navigator.userAgent) && document.location.protocol.substr(0,4) != "http") {
+		cnv.style.display='none';
+		cnv.offsetHeight;
+		cnv.style.display='block';
+	}
 }
 
 
 function evMousemove(e){
-	e.preventDefault(); 
+	e.preventDefault();
+	if (touchScreen) e = e.touches[0];
 	if(!onMove) return;
 	var x=localx(e.clientX), y=localy(e.clientY);
 	onMove(x,y);
 }
 
 function evMouseup(e){
-	e.preventDefault(); 
+	e.preventDefault();
 	if(!onEnd) return;
 	onEnd();
 }
 
 function localx(gx){return (gx/zoom-frame.getBoundingClientRect().left);}
 function localy(gy){
-	return (gy/zoom+shiftTop-frame.getBoundingClientRect().top);
+	return (gy/zoom-frame.getBoundingClientRect().top);
 }
-
